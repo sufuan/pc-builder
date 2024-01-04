dec8ae1ae8e2efafb584fb1014253c299b5c7f76
sufuan <abu49539@gmail.com>
2024-01-04 21:43:59 +0600

    category_products

--- src/pages/monitor.js
+++ src/pages/monitor.js
@@ -1,5 +1,40 @@
-import React from "react";
+import RootLayout from "@/components/Layout";
+import Product from "@/components/UI/Product";
 
-export default function Monitor() {
-  return <div>Monitor</div>;
-}
+const Monitor = ({ products }) => {
+  const productCategory = products[0]?.category;
+  console.log(productCategory);
+  return (
+    <div className="container mx-auto my-10">
+      <h1 className="font-bold text-2xl uppercase text-center py-4">
+        Specific Products: {productCategory}
+      </h1>
+      <div className="flex flex-wrap mx-3">
+        {products?.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Monitor;
+
+Monitor.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://pc-builder-server-black.vercel.app/products?category=Monitor"
+  );
+  const data = await res.json();
+  const products = data.data;
+  console.log(products);
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
