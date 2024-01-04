069378328a2bf908cb04ff1c7f3ea9185b8b8112
sufuan <abu49539@gmail.com>
2024-01-04 21:25:35 +0600

    category_products

--- src/pages/cpu.js
+++ src/pages/cpu.js
@@ -1,7 +1,38 @@
-import React from "react";
+// import RootLayout from "@/components/Layouts";
+import RootLayout from "@/components/Layout";
+import Product from "@/components/UI/Product";
 
-function Cpu() {
-  return <div>Cpu</div>;
-}
+const CPU = ({ products }) => {
+  const productCategory = products[0]?.category;
+  return (
+    <div className="container mx-auto my-10">
+      <h1 className="font-bold text-2xl uppercase text-center py-4">
+        Specific Products: {productCategory}
+      </h1>
+      <div className="flex flex-wrap mx-3">
+        {products?.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Cpu;
+export default CPU;
+
+CPU.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://pc-builder-server-black.vercel.app/products?category=CPU"
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      products: data?.data,
+    },
+  };
+};
